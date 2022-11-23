d961e3b52897fc12bcdd77821e758437be690b76
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-11-23 06:44:35 -0700

    cleanup after master rebase

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -3,3 +3,4 @@ export { getConfig } from "./procedures/getConfig.ts";
 export { health } from "./procedures/health.ts";
 export { properties } from "./procedures/properties.ts";
 export { migration } from "./procedures/migrations.ts";
+export { main } from "./procedures/main.ts";
