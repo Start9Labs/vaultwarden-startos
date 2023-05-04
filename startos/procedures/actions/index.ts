7950081f69e16d1bcffee244ab89db00600e1fcf
BluJ <mogulslayer@gmail.com>
2023-05-04 10:58:25 -0600

    chore: Remove the unused action

--- startos/procedures/actions/index.ts
+++ startos/procedures/actions/index.ts
@@ -1,7 +1,6 @@
-import { nameToLogs } from './nameToLogs'
 import { setupActions } from '@start9labs/start-sdk/lib/actions/setupActions'
 
 /**
  * Add each new Action as the next argument to this function
  */
-export const { actions, actionsMetadata } = setupActions(nameToLogs)
+export const { actions, actionsMetadata } = setupActions()
