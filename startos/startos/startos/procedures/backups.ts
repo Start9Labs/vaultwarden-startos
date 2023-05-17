348a50c15bf1c9516e1e195eb3f3436a0f297f7a
BluJ <mogulslayer@gmail.com>
2023-05-17 14:24:02 -0600

    fix: Type is better

---
+++ startos/startos/startos/procedures/backups.ts
@@ -0,0 +1,6 @@
+import { sdk } from '../sdk'
+
+/**
+ * Here we define what volumes from the Manifest to include in backups
+ */
+export const { createBackup, restoreBackup } = sdk.setupBackups('main')
