afd0bc3027899e657aca770f21329a5d65c22840
Mariusz Kogen <k0gen@pm.me>
2025-06-17 15:24:46 +0200

    fix: import from storeJson

--- startos/install/versionGraph.ts
+++ startos/install/versionGraph.ts
@@ -1,12 +1,12 @@
 import { VersionGraph } from '@start9labs/start-sdk'
 import { current, other } from './versions'
-import { store } from '../fileModels/store.json'
+import { storeJson } from '../fileModels/store.json'
 
 export const versionGraph = VersionGraph.of({
   current,
   other,
   preInstall: async (effects) => {
-    await store.write(effects, {
+    await storeJson.write(effects, {
       ADMIN_TOKEN: '',
       DOMAIN: '',
       smtp: { selection: 'disabled', value: {} },
