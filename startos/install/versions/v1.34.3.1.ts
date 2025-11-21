c13158ca77b3a3ec25b987c001602397a15c5afe
Mariusz Kogen <k0gen@pm.me>
2025-11-21 17:51:32 +0100

    sdk.beta.44 bump

--- startos/install/versions/v1.34.3.1.ts
+++ startos/install/versions/v1.34.3.1.ts
@@ -1,12 +1,12 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const v1_34_3_1 = VersionInfo.of({
-  version: '1.34.3:1-alpha.1',
+  version: '1.34.3:1-alpha.2',
   releaseNotes: `\
 Updated for StartOS 0.4.0
 
 ### Dependency Updates
-*   Updated SDK to beta.43
+*   Updated SDK to beta.44
 *   Updated interface name for backwards compatibility
 *   Updated dev dependencies to latest versions`,
   migrations: {
