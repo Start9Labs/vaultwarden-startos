7c4754acc02a132cfffdab6c95e16261dbe2763f
gStart9 <george@start9labs.com>
2026-02-23 17:10:42 -0700

    Update to v1.35.4

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -3,7 +3,7 @@ import { manifest } from "../generated/manifest.ts";
 
 export const migration: T.ExpectedExports.migration = migrations.fromMapping(
   {
-    // 1.35.3 No migration needed
+    // 1.35.4 No migration needed
   },
   manifest.version,
 );
